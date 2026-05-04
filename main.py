a40d4d9e034826b39616ac82337a504dec187679
zswanner <shawnwanner27@gmail.com>
2026-05-04 14:18:01 -0400

    I updated the format of the objects and started working of the stat calculation logic in a separate file

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import json
+
 import customtkinter as ctk
 
 
-#ALWAYS USE .UPPER
+
 
 def write_json(data):
     with open("stats.json", "w") as file:
@@ -15,17 +16,62 @@ def read_json():
     except FileNotFoundError:
         return None
 
+def to_dict(self):
+    return self.__dict__
 
 
 
 
-class Game():
-    def __init__(self, date, homeTeam, awayTeam):
+class PlayerGameStats:
+    def __init__(self, name, team, date):
+        self.name = name.upper()
+        self.team = team.upper()
         self.date = date
-        self.homeTeam = homeTeam
-        self.awayTeam = awayTeam
-        self.
+
+        self.atBats = 0
+        self.hits = 0
+        self.walks = 0
+        self.strikeouts = 0
+        self.singles = 0
+        self.doubles = 0
+        self.triples = 0
+        self.homeRuns = 0
+
+        self.pitchCount = 0
+        self.pitchingStrikeouts = 0
+        self.pitchingWalks = 0
+        self.pitchingHits = 0
+
+    def to_dict(self):
+        return self.__dict__
 
 
-class PlayerStatsPerGame:
-    def __init__(self,):
+class Player:
+    def __init__(self, name, team):
+        self.name = name.upper()
+        self.team = team.upper()
+        self.games = []
+
+    def add_game(self, game_stats):
+        self.games.append(game_stats.to_dict())
+
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "team": self.team,
+            "games": self.games
+        }
+
+
+class Game:
+    def __init__(self, date, home_team, away_team):
+        self.date = date
+        self.homeTeam = home_team.upper()
+        self.awayTeam = away_team.upper()
+        self.homeScore = 0
+        self.awayScore = 0
+
+    def to_dict(self):
+        return self.__dict__
+
+
