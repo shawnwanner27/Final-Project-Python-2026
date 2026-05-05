25ff44c8923fcffd812ae3ac40cbc8794e94e5d1
zswanner <shawnwanner27@gmail.com>
2026-05-05 14:29:06 -0400

    I recoded the objects again because it wouldnt have worked well

--- main.py
+++ main.py
@@ -2,76 +2,75 @@ import json
 
 import customtkinter as ctk
 
-
-
-
-def write_json(data):
-    with open("stats.json", "w") as file:
-        json.dump(data, file, indent=4)
-
-def read_json():
-    try:
-        with open("stats.json", "r") as file:
-            return json.load(file)
-    except FileNotFoundError:
-        return None
-
-def to_dict(self):
-    return self.__dict__
-
-
-
-
-class PlayerGameStats:
-    def __init__(self, name, team, date):
-        self.name = name.upper()
-        self.team = team.upper()
-        self.date = date
-
-        self.atBats = 0
-        self.hits = 0
-        self.walks = 0
-        self.strikeouts = 0
-        self.singles = 0
-        self.doubles = 0
-        self.triples = 0
-        self.homeRuns = 0
-
-        self.pitchCount = 0
-        self.pitchingStrikeouts = 0
-        self.pitchingWalks = 0
-        self.pitchingHits = 0
-
-    def to_dict(self):
-        return self.__dict__
-
-
 class Player:
     def __init__(self, name, team):
         self.name = name.upper()
         self.team = team.upper()
         self.games = []
 
-    def add_game(self, game_stats):
-        self.games.append(game_stats.to_dict())
+    def add_game(self, stats):
+        self.games.append(stats)
 
-    def to_dict(self):
-        return {
-            "name": self.name,
-            "team": self.team,
+    def get_totals(self):
+        totals = {
+            "atBats": 0,
+            "hits": 0,
+            "walks": 0,
+            "strikeouts": 0,
+            "singles": 0,
+            "doubles": 0,
+            "triples": 0,
+            "homeRuns": 0,
+            "pitchCount": 0,
+            "pitchingStrikes": 0,
+            "pitchingBalls": 0,
+            "pitchingHits": 0}
+
+        for game in self.games:
+            for x in totals:
+                totals[x] += game.get(x, 0)
+
+        return totals
+
+
+
+class League:
+    def __init__(self):
+        self.teams = {}
+        self.players = {}
+        self.games = []
+
+        self.load()
+
+
+    def load(self):
+        try:
+            with open("stats.json", "r") as f:
+                data = json.load(f)
+
+                self.teams = data["teams"]
+                self.games = data["games"]
+
+                for name, pdata in data["players"].items():
+                    p = Player(name, pdata["team"])
+                    p.games = pdata["games"]
+                    self.players[name] = p
+
+        except:
+            pass
+
+    def save(self):
+        data = {
+            "teams": self.teams,
+            "players": {},
             "games": self.games
         }
 
+        for name, p in self.players.items():
+            data["players"][name] = {
+                "team": p.team,
+                "games": p.games
+            }
 
-class Game:
-    def __init__(self, date, home_team, away_team):
-        self.date = date
-        self.homeTeam = home_team.upper()
-        self.awayTeam = away_team.upper()
-        self.homeScore = 0
-        self.awayScore = 0
-
-    def to_dict(self):
-        return self.__dict__
-
-
+        with open("stats.json", "w") as f:
+            json.dump(data, f, indent=4)
